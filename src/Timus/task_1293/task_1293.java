fef4343c236b2085859e4ce9c5d206807b4d799d
evsmat <evsmat@mail.ru>
2022-11-27 16:09:09 +0500

    lr1

--- src/Timus/task_1293/task_1293.java
+++ src/Timus/task_1293/task_1293.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class task_1293 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите количество панелей:");
         int n = in.nextInt();
-        System.out.println("Введите длину панелей:");
         int a = in.nextInt();
-        System.out.println("Введите ширину панелей:");
         int b = in.nextInt();
-        System.out.println(a*b*n*2);
+        System.out.print(a*b*n*2);
     }
 }
