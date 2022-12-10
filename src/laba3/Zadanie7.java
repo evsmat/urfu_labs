cb11e94fc0a0f482ca75dd88e1f4b86cbe382b8d
evsmat <evsmat@mail.ru>
2022-12-10 15:19:21 +0500

    Задание 7 лабораторной работы №3

--- src/laba3/Zadanie7.java
+++ src/laba3/Zadanie7.java
@@ -1,10 +1,21 @@
 package laba3;
 
-import java.util.Scanner;
-
 public class Zadanie7 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
+        int count = 10;
+        char[] letters = new char[count];
+        char letter = 'A';
+        for (int i = 0; i < count; i++) {
+            letters[i] = letter;
+            if (letter == 'Y') letter = 'A';
+            else letter += 2;
+        }
+        for (int i = 0; i < letters.length; i++) {
+            System.out.print(letters[i] + " ");
+        }
+        System.out.println();
+        for (int i = letters.length-1; i >= 0; i--) {
+            System.out.print(letters[i] + " ");
+        }
     }
 }
